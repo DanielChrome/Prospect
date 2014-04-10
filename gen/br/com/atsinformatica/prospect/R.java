df94e7a2880fbe654f017ad74614315bd49273f1
unknown <DanielCosta@ATS0000158.atsmoc.local>
2014-04-10 18:01:45 -0300

    Criando tela principal

--- gen/br/com/atsinformatica/prospect/R.java
+++ gen/br/com/atsinformatica/prospect/R.java
@@ -1077,8 +1077,13 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
-        public static final int logo=0x7f020058;
+        public static final int client_add=0x7f020057;
+        public static final int client_list=0x7f020058;
+        public static final int email=0x7f020059;
+        public static final int export=0x7f02005a;
+        public static final int ic_launcher=0x7f02005b;
+        public static final int logo=0x7f02005c;
+        public static final int settings=0x7f02005d;
     }
     public static final class id {
         public static final int TextView1=0x7f050069;
@@ -1099,7 +1104,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050086;
+        public static final int action_settings=0x7f05008c;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1160,6 +1165,12 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int imageView1=0x7f050086;
+        public static final int imageView2=0x7f050087;
+        public static final int imageView3=0x7f050088;
+        public static final int imageView4=0x7f050089;
+        public static final int imageView5=0x7f05008a;
+        public static final int imageView6=0x7f05008b;
         public static final int listCod=0x7f050080;
         public static final int listEmail=0x7f050082;
         public static final int listMode=0x7f050001;
@@ -1275,7 +1286,8 @@ containing a value of this type.
         public static final int fragment_principal=0x7f03001d;
         public static final int item_list=0x7f03001e;
         public static final int lista=0x7f03001f;
-        public static final int support_simple_spinner_dropdown_item=0x7f030020;
+        public static final int principal=0x7f030020;
+        public static final int support_simple_spinner_dropdown_item=0x7f030021;
     }
     public static final class menu {
         public static final int principal=0x7f0c0000;
